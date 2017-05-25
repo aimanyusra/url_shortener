a1b0e266683ca6160bd373606123a90d870baf4b
Aiman Yusra <aimanyusra@hotmail.com>
2017-05-25 12:51:59 +0800

    added delete link

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -13,7 +13,7 @@ class UrlsController < ApplicationController
 	end
 
 	def create
-		
+
 		@url = Url.new(url_params)
 		@url.shorten
 		if @url.save
@@ -23,9 +23,17 @@ class UrlsController < ApplicationController
 		end   
 	end
 
+	def destroy
+	    @url = Url.find(params[:id])
+	    @url.destroy
+	    redirect_to urls_path
+	end
+
 	private 
 
 	def url_params
 	    params.require(:url).permit(:long_url)
 	end
+
+
 end
